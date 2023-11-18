cb9ef65235684f9a9f5f4e18232bbd52ede30522
Sebastian Fijałkowski <fijalkowskiweb@gmail.com>
2023-11-18 11:44:24 +0100

    added location is new object instead of reference

--- frontend/src/app/services/add-location-to-list.service.ts
+++ frontend/src/app/services/add-location-to-list.service.ts
@@ -13,6 +13,21 @@ export class AddLocationToListService {
   addLocation(location: GeocodingResponse , lat: number, lng: number) {
     location.lat = lat;
     location.lon = lng;
-    this.newLocation.next(location);
+
+    this.newLocation.next({
+      boundingbox: location.boundingbox,
+      class: location.class,
+      display_name: location.display_name,
+      importance: location.importance,
+      lat: location.lat,
+      licence: location.licence,
+      lon: location.lon,
+      osm_id: location.osm_id,
+      osm_type: location.osm_type,
+      place_id: location.place_id,
+      powered_by: location.powered_by,
+      type: location.type
+    });
+
   }
 }
