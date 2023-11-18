8e02041f6ace778ff172413dcecf041f280ec3e4
Sebastian Fijałkowski <fijalkowskiweb@gmail.com>
2023-11-18 12:15:28 +0100

    Change place info if user drag marker

--- frontend/src/app/services/add-location-to-list.service.ts
+++ frontend/src/app/services/add-location-to-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {GeocodingResponse} from "../models/geocoding-response";
 import {Subject} from "rxjs";
+import {GeocodingAPIService} from "./geocoding-api.service";
 
 @Injectable({
   providedIn: 'root'
@@ -8,26 +9,48 @@ import {Subject} from "rxjs";
 export class AddLocationToListService {
   newLocation = new Subject<GeocodingResponse>();
   newLocationAppeared = this.newLocation.asObservable()
-  constructor() { }
+  constructor(private geocodingAPIService: GeocodingAPIService) { }
 
   addLocation(location: GeocodingResponse , lat: number, lng: number) {
-    location.lat = lat;
-    location.lon = lng;
+    if(location.lat == lat && location.lon == lng){
+      location.lat = lat;
+      location.lon = lng;
 
-    this.newLocation.next({
-      boundingbox: location.boundingbox,
-      class: location.class,
-      display_name: location.display_name,
-      importance: location.importance,
-      lat: location.lat,
-      licence: location.licence,
-      lon: location.lon,
-      osm_id: location.osm_id,
-      osm_type: location.osm_type,
-      place_id: location.place_id,
-      powered_by: location.powered_by,
-      type: location.type
-    });
+      this.newLocation.next({
+        boundingbox: location.boundingbox,
+        class: location.class,
+        display_name: location.display_name,
+        importance: location.importance,
+        lat: location.lat,
+        licence: location.licence,
+        lon: location.lon,
+        osm_id: location.osm_id,
+        osm_type: location.osm_type,
+        place_id: location.place_id,
+        powered_by: location.powered_by,
+        type: location.type
+      });
+    }else {
+      this.geocodingAPIService.getLocationByLatLon(lat,lng).subscribe((l) => {
+        this.newLocation.next({
+          boundingbox: l.boundingbox,
+          class: l.class,
+          display_name: l.display_name,
+          importance: l.importance,
+          lat: l.lat,
+          licence: l.licence,
+          lon: l.lon,
+          osm_id: l.osm_id,
+          osm_type: l.osm_type,
+          place_id: l.place_id,
+          powered_by: l.powered_by,
+          type: l.type
+        });
+      })
+    }
+
+
+
 
   }
 }
--- frontend/src/app/services/geocoding-api.service.ts
+++ frontend/src/app/services/geocoding-api.service.ts
@@ -7,12 +7,18 @@ import {GeocodingResponse} from "../models/geocoding-response";
   providedIn: 'root'
 })
 export class GeocodingAPIService {
-  private geocodingURL = "https://geocode.maps.co/search?q="; //example = https://geocode.maps.co/search?q={address}
+  private forwardGeocodeURL = "https://geocode.maps.co/search?q="; //example = https://geocode.maps.co/search?q={address}
+  private reverseGeocodeURL = "https://geocode.maps.co/reverse?"; //example = https://geocode.maps.co/reverse?lat={latitude}&lon={longitude}
   constructor(private http: HttpClient) { }
 
   getLocations(locationName: string) :Observable<GeocodingResponse[]> {
-    let query = locationName.replace(/\s+/g, '+');
-    return this.http.get<GeocodingResponse[]>(this.geocodingURL + "{" + query + "}")
+    const query = locationName.replace(/\s+/g, '+');
+    return this.http.get<GeocodingResponse[]>(this.forwardGeocodeURL + "{" + query + "}")
+  }
+
+  getLocationByLatLon(lat:number, lon: number): Observable<GeocodingResponse>{
+    const requestURL = this.reverseGeocodeURL + "lat=" + lat + "&lon=" + lon + "";
+    return this.http.get<GeocodingResponse>(requestURL);
   }
 
 }
