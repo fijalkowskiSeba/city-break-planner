3846b6552add755fb1385b03639622cac6b5c0e3
Sebastian Fijałkowski <fijalkowskiweb@gmail.com>
2023-11-18 19:22:42 +0100

    geocoding response simplification

--- frontend/src/app/models/geocoding-response.ts
+++ frontend/src/app/models/geocoding-response.ts
@@ -1,19 +1,5 @@
 export interface GeocodingResponse {
-  "place_id": number;
-  "licence": string;
-  "powered_by": string;
-  "osm_type": string;
-  "osm_id": number;
-  "boundingbox": [
-    number,
-    number,
-    number,
-    number
-  ];
   "lat": number;
   "lon": number;
   "display_name": string;
-  "class": string;
-  "type": string;
-  "importance": number;
 }
--- frontend/src/app/services/add-location-to-list.service.ts
+++ frontend/src/app/services/add-location-to-list.service.ts
@@ -17,34 +17,16 @@ export class AddLocationToListService {
       location.lon = lng;
 
       this.newLocation.next({
-        boundingbox: location.boundingbox,
-        class: location.class,
         display_name: location.display_name,
-        importance: location.importance,
         lat: location.lat,
-        licence: location.licence,
         lon: location.lon,
-        osm_id: location.osm_id,
-        osm_type: location.osm_type,
-        place_id: location.place_id,
-        powered_by: location.powered_by,
-        type: location.type
       });
     }else {
       this.geocodingAPIService.getLocationByLatLon(lat,lng).subscribe((l) => {
         this.newLocation.next({
-          boundingbox: l.boundingbox,
-          class: l.class,
           display_name: l.display_name,
-          importance: l.importance,
           lat: l.lat,
-          licence: l.licence,
           lon: l.lon,
-          osm_id: l.osm_id,
-          osm_type: l.osm_type,
-          place_id: l.place_id,
-          powered_by: l.powered_by,
-          type: l.type
         });
       })
     }
