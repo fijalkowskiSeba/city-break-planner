1979633797dcf0c59050d969d47ecb6171a052de
Sebastian Fijałkowski <fijalkowskiweb@gmail.com>
2023-11-27 20:45:19 +0100

    temporary proxy change

--- frontend/src/proxt.conf.prod.js
+++ frontend/src/proxt.conf.prod.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api', '/oauth2', '/login'],
-    target: 'https://city-break-planner.sebastian-fijalkowski.online:8443',
+    target: 'http://city-break-planner.sebastian-fijalkowski.online:8080',
     secure: true,
     logLevel: 'debug'
   }
